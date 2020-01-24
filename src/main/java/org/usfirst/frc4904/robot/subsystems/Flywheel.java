fd35775bd02626c91d14c4ba8fef2da3c4fe193d
Exr0n <spotyie@gmail.com>
2020-01-23 16:23:13 -0800

    update `standard` submodule ref; replace flywheel Replaced flywheel with original version

--- src/main/java/org/usfirst/frc4904/robot/subsystems/Flywheel.java
+++ src/main/java/org/usfirst/frc4904/robot/subsystems/Flywheel.java
@@ -1,44 +1,91 @@
 package org.usfirst.frc4904.robot.subsystems;
 
-import org.usfirst.frc4904.standard.LogKitten;
-import org.usfirst.frc4904.standard.custom.motioncontrollers.MotionController;
-import org.usfirst.frc4904.standard.custom.sensors.InvalidSensorException;
-import org.usfirst.frc4904.standard.subsystems.motor.VelocitySensorMotor;
-
 import edu.wpi.first.wpilibj.SpeedController;
 
-public class Flywheel extends VelocitySensorMotor {
-  // this should take in a solenoidSet
-  private final double BOX_SHOT_SPEED = -1;
-  private final double SPEED_ERROR_THRESHOLD = -1;
-  private double targetSpeed;
+import org.usfirst.frc4904.standard.custom.motioncontrollers.MotionController;
+import org.usfirst.frc4904.standard.subsystems.motor.VelocitySensorMotor;
+import org.usfirst.frc4904.standard.subsystems.motor.speedmodifiers.SpeedModifier;
 
-  public Flywheel(MotionController motionController, SpeedController... speedControllers) {
-    super(motionController, speedControllers);
+/**
+ * Flywheel Base Class
+ * 
+ * ! This class does not include a piston, use Shooter.java for the 2020-season
+ * ! specific flywheel subsystem.
+ * 
+ * This class is designed to go into standard, eventually.
+ */
+public class Flywheel extends VelocitySensorMotor {
+  public enum FlywheelStatus {
+    SPINNING_UP, AT_SPEED
   }
 
-  public void spinDown() {
-    this.targetSpeed = 0.0;
-    super.set(targetSpeed);
+  public static final double boxShotSpeed = 10.0; // TODO: figure out the speed needed for the box shot
+
+  protected FlywheelStatus currentStatus = FlywheelStatus.AT_SPEED;
+  protected double targetSpeed = 0.0;
+
+  /**
+   * Flywheel constructor - extends VelocitySensorMotor
+   * motionController.absoluteTolerance should be set.
+   * 
+   * @param name             The name of the flywheel
+   * @param SpeedModifier    The speedModifier
+   * @param motionController The motionController
+   * @param motors           Motors to control
+   */
+  public Flywheel(String name, SpeedModifier speedModifier, MotionController motionController,
+      SpeedController... motors) {
+    super(name, speedModifier, motionController, motors);
+    super.enableMotionController();
+  }
+
+  /**
+   * Flywheel constructor - extends VelocitySensorMotor
+   * 
+   * @param SpeedModifier    The speedModifier
+   * @param motionController The motionController
+   * @param motors           Motors to control
+   */
+  public Flywheel(SpeedModifier speedModifier, MotionController motionController, SpeedController... motors) {
+    this("Flywheel", speedModifier, motionController, motors);
+  }
+
+  protected void syncStatus() {
+    if (motionController.onTarget()) {
+      currentStatus = FlywheelStatus.AT_SPEED;
+    } else {
+      currentStatus = FlywheelStatus.SPINNING_UP;
+    }
+  }
+
+  public FlywheelStatus getStatus() {
+    syncStatus();
+    return currentStatus;
   }
 
   public boolean isSpunUp() {
-    try {
-      if (Math.abs(super.motionController.getSafely() - this.targetSpeed) < SPEED_ERROR_THRESHOLD) {
-        return true;
-      }
-    } catch (InvalidSensorException e) {
-      LogKitten.e(e.getMessage());
-    }
     return false;
   }
 
-  public void spinUpForDistance(double distance) {
-    // TODO: Calculate RPM
+  public double getSpeed() {
+    return motionController.getSensorValue();
   }
 
-  public void spinUpForBoxShot() {
-    this.targetSpeed = BOX_SHOT_SPEED;
+  public double getTargetSpeed() {
+    return targetSpeed;
+  }
+
+  public void setSpeed() {
     super.set(targetSpeed);
+    syncStatus();
+  }
+
+  public void setSpeed(double speed) {
+    targetSpeed = speed;
+    setSpeed();
+  }
+
+  public void setSpeedForDistance(double distance) {
+    setSpeed(2 * distance); // TODO: set speed value based on distance
   }
 }
