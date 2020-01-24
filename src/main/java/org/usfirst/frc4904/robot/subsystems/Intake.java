f7501257086476aea426090351f950c74475580e
Exr0n <spotyie@gmail.com>
2020-01-23 17:02:53 -0800

    update Intake.java for new OOP model intake now wraps the intake rollers, the funnel motor, and the intake extension solenoid added different default speed constants added various speed setters made subsystem data members public so commands can access them

--- src/main/java/org/usfirst/frc4904/robot/subsystems/Intake.java
+++ src/main/java/org/usfirst/frc4904/robot/subsystems/Intake.java
@@ -1,40 +1,53 @@
 package org.usfirst.frc4904.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
 import org.usfirst.frc4904.standard.subsystems.SolenoidSubsystem;
 import org.usfirst.frc4904.standard.subsystems.SolenoidSubsystem.SolenoidState;
 import org.usfirst.frc4904.standard.subsystems.motor.Motor;
 
-public class Intake extends SubsystemBase {
-  private static final double DEFAULT_ON_SPEED = 0.5; // TODO: test this value
+public class Intake {
+  private static final double DEFAULT_INTAKE_SPEED = 0.5; // TODO: test this value
+  private static final double DEFAULT_FUNNEL_SPEED = 0.5; // TODO: test this value
   private static final double DEFAULT_OFF_SPEED = 0.0;
 
-  protected final Motor intake;
-  protected final SolenoidSubsystem solenoid;
+  public final Motor intake;
+  public final Motor funnel;
+  public final SolenoidSubsystem solenoid;
 
   /**
-   * Intake - Wraps the three intake SRXs (front intake, funnel, belt) and the
+   * Intake - Wraps the two intake SRXs (front intake and the funnel) and the
    * intake solenoid
    *
    * @param intakeMotor    The front intake motor that extends with the intake
    *                       solenoid
+   * @param funnelMotor    The funnel motor
    * @param intakeSolenoid The solenoid that controls the position of the
    *                       intakeMotor rollers
    */
-  Intake(Motor intakeMotor, SolenoidSubsystem intakeSolenoid) {
-    super();
-    setName("Intake");
+  Intake(Motor intakeMotor, Motor funnelMotor, SolenoidSubsystem intakeSolenoid) {
     intake = intakeMotor;
+    funnel = funnelMotor;
     solenoid = intakeSolenoid;
   }
 
-  public void setSpeed(double speed) {
+  public void setIntakeSpeed(double speed) {
     intake.set(speed);
   }
 
+  public void setFunnelSpeed(double speed) {
+    funnel.set(speed);
+  }
+
+  public void setSpeed(double intakeSpeed, double funnelSpeed) {
+    setIntakeSpeed(intakeSpeed);
+    setFunnelSpeed(funnelSpeed);
+  }
+
+  public void setSpeed(double speed) {
+    setSpeed(speed, speed);
+  }
+
   public void start() {
-    setSpeed(DEFAULT_ON_SPEED);
+    setSpeed(DEFAULT_INTAKE_SPEED, DEFAULT_FUNNEL_SPEED);
   }
 
   public void stop() {
