b883900eb1c7d22fe3c70176223a96b54b491cdd
FlyN Nick <nickassader@gmail.com>
2020-01-26 15:51:38 -0800

    StartPanelMotor and StopPanelMotor have been finished! :) (other than one constant that needs to be determined.

--- src/main/java/org/usfirst/frc4904/robot/commands/StartPanelMotor.java
+++ src/main/java/org/usfirst/frc4904/robot/commands/StartPanelMotor.java
@@ -1,12 +1,22 @@
 package org.usfirst.frc4904.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import org.usfirst.frc4904.standard.subsystems.motor.Motor;
 import org.usfirst.frc4904.robot.RobotMap;
 
 /**
  * A command that will start the control panel motor (pretty self-explanatory).
  */
 public class StartPanelMotor extends CommandBase {
-	public StartPanelMotor(RobotMap robotMap) {
+	/**
+	 * @param motor
+	 */
+	public StartPanelMotor(Motor motor) {
+		Double speed = -1d; // TODO: determine the actual optimal speed for the control panel motor
+		motor.set(speed);
+	}
+	public StartPanelMotor() {
+        Double speed = -1d; // TODO: determine the actual optimal speed for the control panel motor
+		RobotMap.Component.controlPanel.set(speed);
 	}
 }
--- src/main/java/org/usfirst/frc4904/robot/commands/StopPanelMotor.java
+++ src/main/java/org/usfirst/frc4904/robot/commands/StopPanelMotor.java
@@ -1,12 +1,20 @@
 package org.usfirst.frc4904.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import org.usfirst.frc4904.standard.subsystems.motor.Motor;
 import org.usfirst.frc4904.robot.RobotMap;
 
 /**
- * A command that will stop the control panel motor (pretty self-explanatory).
+ * A command that will start the control panel motor (pretty self-explanatory).
  */
 public class StopPanelMotor extends CommandBase {
-    public StopPanelMotor(RobotMap robotMap) {
+    /**
+     * @param motor
+     */
+    public StopPanelMotor(Motor motor) {
+        motor.stopMotor();
+    }
+    public StopPanelMotor() {
+        RobotMap.Component.controlPanel.stopMotor();
     }
 }
