fd5d4e8b7477214eb056ab90d01fb6a40ffe5603
FlyN Nick <nickassader@gmail.com>
2020-01-26 14:57:16 -0800

    Some commands were moved around

---
+++ src/main/java/org/usfirst/frc4904/robot/commands/StopPanelMotor.java
@@ -0,0 +1,12 @@
+package org.usfirst.frc4904.standard.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import org.usfirst.frc4904.robot.RobotMap;
+
+/**
+ * A command that will stop the control panel motor (pretty self-explanatory).
+ */
+public class StopPanelMotor extends CommandBase {
+    public StopPanelMotor(RobotMap robotMap) {
+    }
+}
