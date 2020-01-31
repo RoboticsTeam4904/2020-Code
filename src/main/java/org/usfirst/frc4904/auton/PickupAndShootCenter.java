5031ff97b9ea34c6673e46f03caf54bf778bd1f9
Casey Manning <kcgrind@gmail.com>
2020-01-31 12:47:31 -0800

    added current pose

--- src/main/java/org/usfirst/frc4904/auton/PickupAndShootCenter.java
+++ src/main/java/org/usfirst/frc4904/auton/PickupAndShootCenter.java
@@ -3,6 +3,7 @@ package org.usfirst.frc4904.auton;
 import java.util.Arrays;
 
 import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.robot.commands.RunIntake;
 import org.usfirst.frc4904.standard.commands.chassis.SimpleSplines;
 
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
@@ -14,9 +15,16 @@ class PickupAndShootCenter extends CommandGroupBase {
     @Override
     public void addCommands(Command... commands) {
         Trajectory goingToPowerCells = RobotMap.Component.sensorDrive
-                .generateQuinticTrajectory(Arrays.asList(Poses.shootingPose, Poses.centerCollection));
-        SimpleSplines splines = new SimpleSplines(RobotMap.Component.sensorDrive, goingToPowerCells);
-        this.andThen(splines);
-    }
+                .generateQuinticTrajectory(Arrays.asList(Poses.currentPos, Poses.centerCollectStart));
+        goingToPowerCells.relativeTo(Poses.currentPos);
+        SimpleSplines approachSpline = new SimpleSplines(RobotMap.Component.sensorDrive, goingToPowerCells);
+        this.andThen(approachSpline);
+        this.andThen(new RunIntake(null)); // TODO: intake isn't in robotmap yet
+        Trajectory collect = RobotMap.Component.sensorDrive
+                .generateQuinticTrajectory(Arrays.asList(Poses.currentPos, Poses.centerCollectEnd));
+        goingToPowerCells.relativeTo(Poses.currentPos);
+        SimpleSplines collectSpline = new SimpleSplines(RobotMap.Component.sensorDrive, goingToPowerCells);
+        this.andThen(collectSpline);
 
+    }
 }
--- src/main/java/org/usfirst/frc4904/auton/Poses.java
+++ src/main/java/org/usfirst/frc4904/auton/Poses.java
@@ -4,7 +4,11 @@ import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 
 class Poses {
-    static Pose2d shootingPose;
-    static Pose2d sideCollection;
-    static Pose2d centerCollection = new Pose2d(20.0, 20.0, new Rotation2d(4));
+    // TODO: calculate positions
+    static final Pose2d shootingPose = new Pose2d(30.0, 20.0, new Rotation2d(4));
+    static final Pose2d sideCollection = new Pose2d(10.0, 20.0, new Rotation2d(4));
+    static final Pose2d centerCollectStart = new Pose2d(20.0, 20.0, new Rotation2d(4));
+    static final Pose2d centerCollectEnd = new Pose2d(20.0, 20.0, new Rotation2d(4));
+
+    static Pose2d currentPos = new Pose2d(); // TODO: Get from localization
 }
