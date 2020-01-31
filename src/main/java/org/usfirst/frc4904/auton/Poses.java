e7e80fbf73922ac18f2c55831b0fc4d905db5df5
Casey Manning <kcgrind@gmail.com>
2020-01-31 14:15:19 -0800

    poses work:

--- src/main/java/org/usfirst/frc4904/auton/Poses.java
+++ src/main/java/org/usfirst/frc4904/auton/Poses.java
@@ -4,12 +4,16 @@ import edu.wpi.first.wpilibj.geometry.Pose2d;
 import edu.wpi.first.wpilibj.geometry.Rotation2d;
 
 class Poses {
+
+    static final int topOfField = 500;
+    static final int startOfField = 28;
+
     // TODO: calculate positions
     static final Pose2d centerCollectStart = new Pose2d(20.0, 20.0, new Rotation2d(4));
     static final Pose2d centerCollectEnd = new Pose2d(20.0, 20.0, new Rotation2d(4));
-    static Pose2d shootingPose;
+    static Pose2d shootingPose = new Pose2d(topOfField - 94.66, 0, new Rotation2d(90));
     static Pose2d sideCollection = new Pose2d(49.04, 25.4, new Rotation2d(5));
     static Pose2d centerCollection = new Pose2d(20.0, 20.0, new Rotation2d(4));
 
-    static Pose2d currentPos = new Pose2d(); // TODO: Get from localization
+    static Pose2d currentPos = new Pose2d();// TODO: Get from localization
 }
