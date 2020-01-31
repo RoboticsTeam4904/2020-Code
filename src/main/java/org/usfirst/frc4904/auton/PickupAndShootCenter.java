7a8aa783a11d00161b968e5ebe59ca47572f8010
Liam <wildrew@nuevaschool.org>
2020-01-31 13:41:32 -0800

    fixed some bugs in pickup and shootcenter

--- src/main/java/org/usfirst/frc4904/auton/PickupAndShootCenter.java
+++ src/main/java/org/usfirst/frc4904/auton/PickupAndShootCenter.java
@@ -3,7 +3,9 @@ package org.usfirst.frc4904.auton;
 import java.util.Arrays;
 
 import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.robot.commands.FlywheelSpinDown;
 import org.usfirst.frc4904.robot.commands.RunIntake;
+import org.usfirst.frc4904.robot.commands.Shoot;
 import org.usfirst.frc4904.standard.commands.chassis.SimpleSplines;
 
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
@@ -29,6 +31,13 @@ class PickupAndShootCenter extends SequentialCommandGroup {
                 .generateQuinticTrajectory(Arrays.asList(Poses.currentPos, Poses.shootingPose));
         SimpleSplines moveToShootSpline = new SimpleSplines(RobotMap.Component.sensorDrive, moveToShoot);
         this.andThen(moveToShootSpline);
+        double FlywheelSpeed = 0.0;
+        andThen(new Shoot(RobotMap.Component.indexer, RobotMap.Component.shooter, FlywheelSpeed));
+        andThen(new FlywheelSpinDown(RobotMap.Component.flywheel));
+        
+
+
+
 
     }
 }
