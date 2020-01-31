36be9a9fdbdd8ca68a93c240009ee0f9e3d47db6
isaanca <isaanca@nuevaschool.org>
2020-01-30 17:38:55 -0800

    write PickupAndShootSide command

--- src/main/java/org/usfirst/frc4904/auton/PickupAndShootSide.java
+++ src/main/java/org/usfirst/frc4904/auton/PickupAndShootSide.java
@@ -1,14 +1,22 @@
 package org.usfirst.frc4904.auton;
 
+import java.util.Arrays;
+
+import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.standard.commands.chassis.SimpleSplines;
+
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandGroupBase;
 
 class PickupAndShootSide extends CommandGroupBase {
-
+    
     @Override
     public void addCommands(Command... commands) {
-        // TODO Auto-generated method stub
-
+        Trajectory goingToPowerCells = RobotMap.Component.sensorDrive
+                .generateQuinticTrajectory(Arrays.asList(Poses.shootingPose, Poses.sideCollection));
+        SimpleSplines splines = new SimpleSplines(RobotMap.Component.sensorDrive, goingToPowerCells);
+        this.andThen(splines);
     }
 
 }
--- src/main/java/org/usfirst/frc4904/auton/Poses.java
+++ src/main/java/org/usfirst/frc4904/auton/Poses.java
@@ -5,6 +5,6 @@ import edu.wpi.first.wpilibj.geometry.Rotation2d;
 
 class Poses {
     static Pose2d shootingPose;
-    static Pose2d sideCollection;
+    static Pose2d sideCollection = new Pose2d(49.04, 25.4, new Rotation2d(5));
     static Pose2d centerCollection = new Pose2d(20.0, 20.0, new Rotation2d(4));
 }
