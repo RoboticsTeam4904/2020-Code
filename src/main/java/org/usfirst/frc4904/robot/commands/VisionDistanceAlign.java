80fb3d94aff62fcbccb328d2e70a77cc4b281fa3
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2020-02-07 16:29:19 -0800

    added vision distance align, the math might need to be changed at somepoint

---
+++ src/main/java/org/usfirst/frc4904/robot/commands/VisionDistanceAlign.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc4904.robot.commands;
+
+import java.util.List;
+
+import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.standard.commands.chassis.SimpleSplines;
+
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.trajectory.Trajectory;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class VisionDistanceAlign extends SequentialCommandGroup {
+    protected static final double DEFAULT_ANGLE = 0.0;
+    protected static final double DEFAULT_DISTANCE = 0.0;
+
+    public VisionDistanceAlign() {
+        double currentAngle = RobotMap.Component.navx.getYaw();
+        double beta = RobotMap.NetworkTables.Vision.beta.getDouble(DEFAULT_ANGLE);
+        double xPose = Math.cos(beta) * RobotMap.NetworkTables.Vision.distanceToTarget.getDouble(DEFAULT_DISTANCE);
+        double yPose = Math.sin(beta) * RobotMap.NetworkTables.Vision.distanceToTarget.getDouble(DEFAULT_DISTANCE);
+
+        Trajectory traj = RobotMap.Component.splinesChassis
+                .generateQuinticTrajectory(List.of(new Pose2d(0, 0, Rotation2d.fromDegrees(currentAngle)),
+                        new Pose2d(xPose, yPose, Rotation2d.fromDegrees(currentAngle - beta))));
+
+        addCommands(new SimpleSplines(RobotMap.Component.splinesChassis, traj));
+    }
+}
