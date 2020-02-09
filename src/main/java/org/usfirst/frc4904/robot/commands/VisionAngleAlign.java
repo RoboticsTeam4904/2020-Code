194acc4e50dd95cdae5a5f0fb57e7b4e721b834f
Nikhil Suresh <nikhilsuresh24@yahoo.com>
2020-02-09 11:35:05 -0800

    updated vision alignment commands with better math

--- src/main/java/org/usfirst/frc4904/robot/commands/VisionAngleAlign.java
+++ src/main/java/org/usfirst/frc4904/robot/commands/VisionAngleAlign.java
@@ -10,6 +10,6 @@ import org.usfirst.frc4904.standard.commands.chassis.ChassisTurn;
 public class VisionAngleAlign extends ChassisTurn {
     protected static final double DEFAULT_ANGLE = 0.0;
     public VisionAngleAlign() {
-        super("VisionAngleAlign", RobotMap.Component.chassis, RobotMap.NetworkTables.Vision.beta.getDouble(DEFAULT_ANGLE), RobotMap.Component.navx, new ChassisIdle("FailedVisionAngleAlign", RobotMap.Component.chassis), RobotMap.Component.turnPID);
+        super("VisionAngleAlign", RobotMap.Component.chassis, RobotMap.NetworkTables.Vision.theta.getDouble(DEFAULT_ANGLE), RobotMap.Component.navx, new ChassisIdle("FailedVisionAngleAlign", RobotMap.Component.chassis), RobotMap.Component.turnPID);
     }
 }
--- src/main/java/org/usfirst/frc4904/robot/commands/VisionDistanceAlign.java
+++ src/main/java/org/usfirst/frc4904/robot/commands/VisionDistanceAlign.java
@@ -18,15 +18,16 @@ public class VisionDistanceAlign extends SequentialCommandGroup {
     protected static final double DEFAULT_DISTANCE = 0.0;
 
     public VisionDistanceAlign() {
-        double currentAngle = RobotMap.Component.navx.getYaw();
         double beta = RobotMap.NetworkTables.Vision.beta.getDouble(DEFAULT_ANGLE);
+        double theta = RobotMap.NetworkTables.Vision.theta.getDouble(DEFAULT_ANGLE);
         double distance = RobotMap.NetworkTables.Vision.distanceToTarget.getDouble(DEFAULT_DISTANCE);
-        double xPose = Math.cos(beta) * distance;
-        double yPose = Math.sin(beta) * distance;
+        double xPose = distance * Math.abs(Math.sin(Math.toRadians(Math.abs(theta) + Math.abs(beta - 90)))) * Math.signum(theta);
+        double yPose = distance * Math.abs(Math.cos(Math.toRadians(Math.abs(theta) + Math.abs(beta - 90))));
+        double deltaAngle = beta - 90;
 
         Trajectory traj = RobotMap.Component.splinesChassis
-                .generateQuinticTrajectory(List.of(new Pose2d(0, 0, Rotation2d.fromDegrees(currentAngle)),
-                        new Pose2d(xPose, yPose, Rotation2d.fromDegrees(currentAngle - beta))));
+                .generateQuinticTrajectory(List.of(new Pose2d(0, 0, Rotation2d.fromDegrees(0)),
+                        new Pose2d(xPose, yPose, Rotation2d.fromDegrees(deltaAngle))));
 
         addCommands(new SimpleSplines(RobotMap.Component.splinesChassis, traj));
     }
