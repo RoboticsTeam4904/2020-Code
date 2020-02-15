f8b61e0da1b65eb6fdc2ff5d2b67e6f0b5455ba6
Casey Manning <kcgrind@gmail.com>
2020-02-14 16:38:12 -0800

    Update SendSplines.java

--- src/main/java/org/usfirst/frc4904/robot/commands/SendSplines.java
+++ src/main/java/org/usfirst/frc4904/robot/commands/SendSplines.java
@@ -1,13 +1,19 @@
 package org.usfirst.frc4904.robot.commands;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import org.usfirst.frc4904.auton.AutonRoutine;
+import org.usfirst.frc4904.standard.commands.chassis.SimpleSplines;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.trajectory.Trajectory;
 import edu.wpi.first.wpilibj.trajectory.Trajectory.State;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.CommandGroupBase;
 
 public class SendSplines extends CommandBase {
     Double[] xPos;
@@ -15,6 +21,20 @@ public class SendSplines extends CommandBase {
 
     public SendSplines(Trajectory spline) {
         List<State> states = spline.getStates();
+        xPos = new Double[states.size()];
+        yPos = new Double[states.size()];
+        for (int i = 0; i < states.size(); i++) {
+            xPos[i] = states.get(i).poseMeters.getTranslation().getX();
+            yPos[i] = states.get(i).poseMeters.getTranslation().getY();
+        }
+    }
+
+    public SendSplines(AutonRoutine autonRoutine) {
+        List<State> states = new ArrayList<State>();
+        for (Command c : autonRoutine.getSplinesList()) {
+            SimpleSplines s = (SimpleSplines) c;
+            states.addAll(s.trajectory.getStates());
+        }
         xPos = new Double[states.size()];
         yPos = new Double[states.size()];
         for (int i = 0; i < states.size(); i++) {
