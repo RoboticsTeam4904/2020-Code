1f06ff96699e556974a5f5ce67ac5c4647fac71c
isaanca <isaanca@nuevaschool.org>
2020-03-08 15:05:57 -0700

    add send method to setleds

--- src/main/java/org/usfirst/frc4904/robot/commands/setLeds.java
+++ src/main/java/org/usfirst/frc4904/robot/commands/setLeds.java
@@ -4,31 +4,39 @@
 // import 
 import org.usfirst.frc4904.standard.custom.CustomCAN;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class setLeds{
-
-
-    // Setting bits in Java for the leds on the robot
-    static byte[] arr1, arr2, arr3, arr4, arr5, arr6, arr7, arr8;
-    static byte[][] fullBytes = {arr1, arr2, arr3, arr4, arr5, arr6, arr7, arr8};
-
-    public setLeds(String name, int id) {
-        //arr1 needs to tell us if there is a custom color, they are ready to shoot, in shooting zone, and the alliance color
-        arr1 = new byte[]{0,0,0,0,0,0,0,0};
-        //arr2 needs to be red color, I set it to 255
-        arr2 = new byte[]{1,1,1,1,1,1,1,1};
-        //arr3 needs to be blue color, I set it to 255
-        arr2 = new byte[]{1,1,1,1,1,1,1,1};
-        //arr4 needs to be green color, I set it to 255
-        arr2 = new byte[]{1,1,1,1,1,1,1,1};
-        //arr5 through arr8 need to be all 0
-        arr5 = new byte[]{0,0,0,0,0,0,0,0};
-        arr6 = new byte[]{0,0,0,0,0,0,0,0};
-        arr7 = new byte[]{0,0,0,0,0,0,0,0};
-        arr8 = new byte[]{0,0,0,0,0,0,0,0};
+public class SetLeds extends CommandBase {
+    public SetLeds(String name, int id) {
+        // Setting bits in Java for the leds on the robot
+        byte[] arr1, arr2, arr3, arr4, arr5, arr6, arr7, arr8;
+        byte[][] fullBytes;
         
-        fullBytes = new byte[][]{arr1, arr2, arr3, arr4, arr5, arr6, arr7, arr8};
+        //first array tells us if there is a custom color, they are ready to shoot, in shooting zone, and the alliance color
+        //second through fourth are the amounts of red, blue, and green respectively
+        //the rest of the arrays need to be all 0
+        fullBytes = new byte[][]{
+            new byte[]{0,0,0,0,0,0,0,0}, 
+            new byte[]{1,1,1,1,1,1,1,1},
+            new byte[]{1,1,1,1,1,1,1,1},
+            new byte[]{1,1,1,1,1,1,1,1},
+            new byte[]{0,0,0,0,0,0,0,0},
+            new byte[]{0,0,0,0,0,0,0,0},
+            new byte[]{0,0,0,0,0,0,0,0},
+            new byte[]{0,0,0,0,0,0,0,0},
+        };
+   }
 
+   public void send(byte[][] fullBytes) {
+        // byte[] arr1, arr2, arr3, arr4, arr5, arr6, arr7, arr8 = fullBytes;
+        // First bit of arr1 determines if there is a custom color
+        int customColor = fullBytes[0][0];
+        if (customColor == 0) {
+            fullBytes[1] = new byte[]{0,0,0,0,0,0,0,0};
+            fullBytes[2] = new byte[]{0,0,0,0,0,0,0,0};
+            fullBytes[3] = new byte[]{0,0,0,0,0,0,0,0};
+        }
+        
         //send data using FRCNetCommCANSessionMuxSendMessage​
         //use https://first.wpi.edu/FRC/roborio/release/docs/java/edu/wpi/first/hal/can/CANJNI.html#FRCNetCommCANSessionMuxSendMessage(int,byte%5B%5D,int) in the future
         CustomCAN can = new CustomCAN(name, id);
@@ -36,6 +44,4 @@ public class setLeds{
             can.write(fullBytes[i]);
         }
    }
-
-
 } 
