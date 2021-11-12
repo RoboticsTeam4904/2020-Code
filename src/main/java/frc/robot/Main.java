7c1d4102f33d7d079984ec2e506e427e8749a535
zbuster05 <41530680+zbuster05@users.noreply.github.com>
2021-11-12 09:48:30 -0800

    Why is there a p tag

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -22,7 +22,7 @@ public final class Main {
   /**
    * Main initialization function. Do not perform any initialization here.
    *
-   * <p>If you change your main robot class, change the parameter type.
+   * If you change your main robot class, change the parameter type.
    */
   public static void main(String... args) {
     RobotBase.startRobot(Robot::new);
